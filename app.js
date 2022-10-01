e128313799f6e330d17581a0c5575fdcb5719316
Alec Santamaria <alec@Alecs-MBP.attlocal.net>
2022-10-01 15:15:51 -0400

    installed dependencies

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(3000)
+const session = require('express-session');
+const MongoForum = require('connect-mongo');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+var flash = require('connect-flash');
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+app.set("views", __dirname + "/views");
+
+app.set("view engine", "hbs");
+
+
+app.listen(3000);
---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/User")
+const bcryptjs = require('bcryptjs');
+
+
+
+
+
+module.exports = router;
